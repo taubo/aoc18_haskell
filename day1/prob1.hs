690a818ed9cc3a39045da40fcb0d70cbba4732e2
tubix <mauro.tubiana@gmail.com>
2019-03-24 01:00:17 +0100

    problem b, day 1

--- day1/prob1.hs
+++ day1/prob1.hs
@@ -1,19 +1,37 @@
 import System.IO
-import Result
+import qualified Data.Set as Set
+-- import Result
 
+rmPlus :: String -> String
 rmPlus str
   | head str == '+' = tail str
   | otherwise = str
 
+toInt :: [String] -> [Int]
 toInt ls = map (readInt . rmPlus) ls
     where readInt = read :: String -> Int
 
-main = do
+probA :: IO ()
+probA = do
     inputFile <- openFile "input" ReadMode
     content <- hGetContents inputFile
     let lin = lines content
         nums = toInt lin
         sums = sum nums
-    putStrLn (resultString 1 1 547)
+    -- putStrLn (resultString 1 1 547)
     putStrLn . show $ sums
     hClose inputFile
+
+findDuplicateFreq (x:xs) set
+    | Set.member x set = x
+    | otherwise = findDuplicateFreq xs (Set.insert x set)
+
+probB :: IO ()
+probB = do
+    inputFile <- openFile "input" ReadMode
+    content <- hGetContents inputFile
+    let lin = lines content
+        nums = toInt lin
+        dupFreq = findDuplicateFreq (scanl (+) 0 (cycle nums)) Set.empty
+    putStrLn "Problem B"
+    putStrLn . show $ dupFreq
